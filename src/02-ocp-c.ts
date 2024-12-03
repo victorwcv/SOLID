4a27be4aa58ed3eb6c3098467fb0c4e3d473868d
William <victorwccv@gmail.com>
2024-12-02 21:59:44 -0500

    Removing axios dependency.

--- src/02-ocp-c.ts
+++ src/02-ocp-c.ts
@@ -1,11 +1,20 @@
-import axios from "axios";
+// import axios from "axios";
 
 export class HttpClient {
 
+  // async get(url: string) {
+  //   const { data, status } = await axios.get(url);
+  //   console.log({status});
+  //   return {data, status};
+  // }
+
   async get(url: string) {
-    const { data, status } = await axios.get(url);
-    console.log({status});
-    return {data, status};
+
+    const resp = await fetch( url );
+    const data = await resp.json();
+
+    
+    return { data, status: resp.status };
   }
 
 }
